3ac8a2fcd54ff0d0cf8adb6795980c1c0c52edba
Gregori Zeidler <126284166+gregorizeidler@users.noreply.github.com>
2025-03-10 10:09:20 -0300

    Update face_analysis.py

--- app/services/face_analysis.py
+++ app/services/face_analysis.py
@@ -1,1 +1,18 @@
+from deepface import DeepFace
+import cv2
+import dlib
 
+def detect_face_swaps_or_edits(image_path):
+    try:
+        analysis = DeepFace.analyze(img_path=image_path, actions=['emotion', 'age', 'gender'])
+        if 'error' in analysis:
+            return True  
+    except:
+        return True  
+    return False
+
+def detect_crop_or_edit(image_path):
+    img = cv2.imread(image_path)
+    detector = dlib.get_frontal_face_detector()
+    faces = detector(img, 1)
+    return len(faces) == 0
